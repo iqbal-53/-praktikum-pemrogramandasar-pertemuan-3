ae4187514ce3aa1fd955738eab371ec1fcd37a86
iqbalramadhani <iqbal.ramadhani53@gmail.com>
2021-10-02 15:22:21 +0700

    first commit

---
+++ Pertemuan3Perkalian/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,hasil;
+
+    printf("Program Perkalian \n\n");
+
+    printf("Masukkan Angka Pertama = ");
+    scanf("%d",&a);
+
+    printf("Masukkan Angka Kedua = ");
+    scanf("%d", &b);
+
+    hasil = a * b;
+    //proses
+
+    printf("Hasil dari perkalian = %d \n", hasil);
+
+    return 0;
+}
---
+++ Pertemuan3Variabel/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int Bilangan1; // variabel Bilangan dengan tipe integer
+	float Bilangan2; // variabel Bilangan dengan tipe real
+
+	printf("Nilai Integer  : ");
+	scanf("%d",&Bilangan1);
+	printf("Nilai Integer Anda : %d \n", Bilangan1);
+
+	printf("\n Nilai Real  : ");
+	scanf("%f",&Bilangan2);
+	printf("Nilai Real Anda : %f", Bilangan2);
+}
